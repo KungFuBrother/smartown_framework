dd16487a2718e2ef91ef654f4bf59081130b1aea
Smartown <1076192306@qq.com>
2016-01-21 14:44:50 +0800

    BaseNotifyActivity Toolbar

--- ui/src/main/java/com/smartown/library/ui/base/BaseNotifyActivity.java
+++ ui/src/main/java/com/smartown/library/ui/base/BaseNotifyActivity.java
@@ -1,6 +1,7 @@
 package com.smartown.library.ui.base;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -13,6 +14,7 @@ import com.smartown.application.R;
  */
 public abstract class BaseNotifyActivity extends BaseActivity {
 
+    private Toolbar toolbar;
     private LinearLayout loadingLayout;
     private FrameLayout contentLayout;
 
@@ -24,9 +26,18 @@ public abstract class BaseNotifyActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base_notify);
-        loadingLayout = (LinearLayout) findViewById(R.id.base_activity_loading_layout);
+        toolbar = (Toolbar) findViewById(R.id.base_activity_toolbar);
         contentLayout = (FrameLayout) findViewById(R.id.base_activity_contnet_layout);
+        loadingLayout = (LinearLayout) findViewById(R.id.base_activity_loading_layout);
         loadingTextView = (TextView) findViewById(R.id.base_activity_loading_text);
+        configToolbar();
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     /**
@@ -42,19 +53,23 @@ public abstract class BaseNotifyActivity extends BaseActivity {
         }
     }
 
-    /**
-     * bind subClass views;
-     *
-     * @param viewId
-     */
-    protected View initViewById(int viewId) {
-        return contentView.findViewById(viewId);
+    public Toolbar getToolbar() {
+        return toolbar;
+    }
+
+    protected void configToolbar() {
     }
 
     /**
      * network mission start
      */
     protected void showLoading() {
+        loadingTextView.setText("请稍候...");
+        loadingLayout.setVisibility(View.VISIBLE);
+    }
+
+    protected void showLoading(String s) {
+        loadingTextView.setText(s);
         loadingLayout.setVisibility(View.VISIBLE);
     }
 
